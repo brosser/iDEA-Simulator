24c5d1a38165c475bb3a8a4b16ded2cd226aa07b
HuiYan Cheah <hycheah@gmail.com>
2013-04-20 10:55:03 +0800

    too interesting to ignore

--- benchmark/toy/crc/crc8.c
+++ benchmark/toy/crc/crc8.c
@@ -1,16 +1,17 @@
-//http://www.pololu.com/docs/0J44/6.7.6
-//http://ghsi.de/CRC/index.php?Polynom=10010001&Message=01+83
+// Website origin: http://www.pololu.com/docs/0J44/6.7.6
+// CRC calculator: http://ghsi.de/CRC/index.php?Polynom=10010001&Message=01+83
+// Conversion algorithms: http://archive.online-convert.com/
 
 #include <stdio.h>
 
-const unsigned int CRC7_POLY = 0x91;
+const unsigned char CRC7_POLY = 0x91;
  
 int main ()
 {
     unsigned int length = 2;
     unsigned int i, j;
-    unsigned int message[3] = {0x83, 0x01, 0x00};
-    unsigned int crc = 0x0;
+    unsigned char message[3] = {0x83, 0x01, 0x00};
+    unsigned char crc = 0x0;
  
     for (i = 0; i < length; i++)
     {
